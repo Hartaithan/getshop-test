b65974751d8f6d1826b5b315e7ead4cd7730079a
hartaithan <hartaithan@gmail.com>
2022-02-05 11:38:41 +0900

    add VideoPage layout

--- src/pages/VideoPage/VideoPage.tsx
+++ src/pages/VideoPage/VideoPage.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./videoPage.scss";
 
 function VideoPage() {
-  return <div>VideoPage</div>;
+  return (
+    <div className="videoPage">
+      <div className="videoPage__badge">
+        <p className="videoPage__badge__title">
+          ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! ПОДАРИТЕ ЕМУ СОБАКУ!
+        </p>
+        <img src="./qr.png" alt="qr code" />
+        <p>{`Сканируйте QR-код\nили нажмите ОК`}</p>
+        <Link to="/form">
+          <button>ОК</button>
+        </Link>
+      </div>
+      <video autoPlay muted controls={false} loop>
+        <source src="./video.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default VideoPage;
